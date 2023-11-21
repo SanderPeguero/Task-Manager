3b33bea08140e47cd9a040728d837e8415320645
jeremy castillo taveras <jeremycastillo901@gmail.com>
2023-11-21 19:11:20 -0400

    Project Clean

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,7 @@ import App from './App.jsx'
 import './index.css'
 import { HashRouter } from 'react-router-dom'
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <HashRouter>
+  <HashRouter>
     <App />
-    </HashRouter>
-  </React.StrictMode>,
+  </HashRouter>
 )
