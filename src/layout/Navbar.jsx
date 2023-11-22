a36e2b6a7991a717eeb19ed051cfaec54df7843c
Daniel8529 <carlosdanielmonterogomez@gmail.com>
2023-11-22 11:35:07 -0400

    Arreglando correcciones

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-const Navbar = ({openmodal,setOpenmodal}) => {
+const Navbar = () => {
   const [open, setOpen] = useState(false);
   const [open2, setOpen2] = useState(false);
 
