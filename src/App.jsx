dd4d223490cfeecf4d05b9f4ce9e393afc536510
jose22gonzalez <urenajosealbeto@gmail.com>
2023-11-22 15:55:30 -0400

    created the logic functions handler with React Context

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,92 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { Context } from './Context'
 import Navbar from './layout/Navbar'
 import { Outlet, Route, Routes } from 'react-router-dom'
 import YourComponent from './layout/Form'
 import ToDoList from './layout/ToDoList'
+
+const task = [
+  {
+      id: 1,
+      title: "Complete monthly report",
+      description: "Prepare and send activity",
+      category: "Work",
+      priority: "High",
+      expirationdate: "2023-12-1",
+      status: "Pending",
+      comments: [1],
+      notes: "Be sure to include key statistics.",
+      responsible: "Juan"
+  },
+  {
+      id: 2,
+      title: "Buy ingredients for dinner",
+      description: "Make a list and buy the ingredients for dinner.",
+      category: "Personal",
+      priority: "medium",
+      expirationdate: "2023-11-25",
+      status: "Pending",
+      comments: [],
+      notes: "",
+      responsible: "Maria",
+  },
+
+  {
+      id: 3,
+      title: "Review code of the new component",
+      description: "Perform a code review of the component before deployment.",
+      category: "Development",
+      priority: "High",
+      expirationdate: "2023-12-05",
+      status: "In Progress",
+      comments: ["Can efficiency be improved here?"],
+      notes: "",
+      responsible: "Carlos",
+  },
+  {
+      id: 4,
+      title: "Exercise",
+      description: "Perform a 30-minute training session.",
+      category: "Health",
+      priority: "Low",
+      expirationdate: "2023-11-26",
+      status: "Pending",
+      comments: [],
+      notes: "Preferably in the morning.",
+      responsible: "Ana",
+  },
+  {
+      id: 5,
+      title: "Set up planning meeting",
+      description: "Coordinate with the team to establish the agenda for the planning meeting.",
+      category: "Work",
+      priority: "medium",
+      expirationdate: "2023-12-02",
+      status: "Pending",
+      comments: ["Can all team members attend?"],
+      grades: "",
+      responsible: "Project Manager",
+  },
+]
+
 function App() {
 
   const [openmodal, setOpenmodal] = useState(true)
   const [ToDo, setToDo] = useState([])
+  const [ToDoEdit, setToDoEdit] = useState({})
+  const [ToDoDeleted, setToDoDeleted] = useState({})
 
+  useEffect(() => {
+    setToDo(task)
+    console.log("App")
+    console.log(ToDo)
+  }, [])
+  
 
   return (
     <>
-     
+     <Context.Provider value={{ToDo, setToDo, ToDoEdit, setToDoEdit, ToDoDeleted, setToDoDeleted}}>
+   
       <header>
         <Navbar openmodal={openmodal} setOpenmodal={setOpenmodal}></Navbar>
       </header>
@@ -27,7 +101,8 @@ function App() {
           <Route path="/" element={""} component={""} />
         </Route>
       </Routes>
-      <Outlet />
+      <Outlet /> 
+       </Context.Provider>
     </>
 
 
--- src/layout/Form.jsx
+++ src/layout/Form.jsx
@@ -1,72 +1,131 @@
-import React from 'react';
+import React, { useState, useContext } from 'react';
+import { Context } from '../Context';
+import { useEffect } from 'react';
 
-const YourComponent = ({openmodal,setOpenmodal}) => {
-    const STOP =(e)=>{
-       e.stopPropagation();
+const YourComponent = ({ openmodal, setOpenmodal }) => {
+    const {ToDo, setToDo, ToDoEdit, task } = useContext(Context)
+    const [Id, setId] = useState(6)
+    const [Title, setTitle] = useState("")
+    const [Category, setCategory] = useState("")
+    const [Description, setDescription] = useState("")
+    const [Priority, setPriority] = useState("")
+    const [ExpiratioDate, setExpiratioDate] = useState("")
+    const [FormToDo, setFormToDo] = useState({
+
+        id: 0,
+        title: "",
+        description: "",
+        category: "",
+        priority: "",
+        expirationdate: "",
+        status: "",
+        comments: [],
+        notes: "",
+        responsible: ""
+
+    })
+
+
+    const AddToDoHandler = (e) => {
+        e.preventDefault();
+       const newTask = {
+            ...FormToDo,
+            id: Id,
+            title: Title,
+            description: Description,
+            category: Category,
+            priority: Priority,
+            expirationdate: ExpiratioDate,
+            status: "Pending",
+            comments: [],
+            notes: "Esto es una Prueba",
+            responsible: "Juan Perez"
+        }
+
+        setFormToDo(newTask)
+
+        setToDo([...ToDo, FormToDo])
+        setId(Id + 1)
+        
+    
     }
-   
-   if(!openmodal){
-    return (
-        <div className='absolute inset-0 flex items-center justify-center z-50' onClick={() => setOpenmodal(true)}>
-        <div className="flex flex-col justify-center mt-12 overflow-hidden" onClick={STOP} >
-            <div className="relative py-3" >
-                <div className="relative px-4 py-8 bg-white mx-8 md:mx-0 shadow rounded-3xl sm:p-8" >
-                    <div className="max-w-md mx-auto" >
-                        <div className="flex items-center space-x-5">
-                            <div className="h-14 w-14 bg-yellow-200 rounded-full flex flex-shrink-0 justify-center items-center text-yellow-500 text-2xl font-mono">i</div>
-                            <div className="block pl-2 font-semibold text-xl self-start text-gray-700">
-                                <h2 className="leading-relaxed">Create an Event</h2>
-                                <p className="text-sm text-gray-500 font-normal leading-relaxed">Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
-                            </div>
-                        </div>
-                        <div className="divide-y divide-gray-200">
-                            <div className="py-6 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
-                                <div className="flex flex-col">
-                                    <label className="leading-loose">Event Title</label>
-                                    <input type="text" className="px-4 py-2 border focus:ring-gray-500 focus:border-gray-900 w-full sm:text-sm border-gray-300 rounded-md focus:outline-none text-gray-600 bg-white" placeholder="Event title" />
-                                </div>
-                                <div className="flex flex-col">
-                                    <label className="leading-loose">Event Subtitle</label>
-                                    <input type="text" className="px-4 py-2 border focus:ring-gray-500 focus:border-gray-900 w-full sm:text-sm border-gray-300 rounded-md focus:outline-none text-gray-600 bg-white" placeholder="Optional" />
-                                </div>
-                                <div className="flex items-center space-x-4">
-                                    <div className="flex flex-col">
-                                        <label className="leading-loose">Start</label>
-                                        <div className="relative focus-within:text-gray-600 text-gray-400">
-                                            <input type="date"
-                                                aria-label="Fecha" className="pr-4 pl-10 py-2 border focus:ring-gray-500 focus:border-gray-900 w-full sm:text-sm border-gray-300 rounded-md focus:outline-none text-gray-600 bg-white" placeholder="25/02/2020" />
-                                        </div>
-                                    </div>
-                                    <div className="flex flex-col">
-                                        <label className="leading-loose">End</label>
-                                        <div className="relative focus-within:text-gray-600 text-gray-400">
-                                            <input type="date"
-                                                aria-label="Fecha" className="pr-4 pl-10 py-2 border focus:ring-gray-500 focus:border-gray-900 w-full sm:text-sm border-gray-300 rounded-md focus:outline-none text-gray-600 bg-white" placeholder="26/02/2020" />
-                                           
-                                        </div>
+
+    useEffect(() => {
+        setTitle(ToDoEdit.title)
+    }, [ToDoEdit])
+    
+
+
+    const STOP = (e) => {
+        e.stopPropagation();
+    }
+
+    if (!openmodal) {
+        return (
+            <div className='absolute inset-0 flex items-center justify-center z-50' onClick={() => setOpenmodal(true)}>
+                <div className="flex flex-col justify-center mt-12 overflow-hidden" onClick={STOP} >
+                    <div className="relative py-3" >
+                        <div className="relative px-4 py-8 bg-white mx-8 md:mx-0 shadow rounded-3xl sm:p-8" >
+                            <div className="max-w-md mx-auto" >
+                                <div className="flex items-center space-x-5">
+                                    <div className="h-14 w-14 bg-yellow-200 rounded-full flex flex-shrink-0 justify-center items-center text-yellow-500 text-2xl font-mono">i</div>
+                                    <div className="block pl-2 font-semibold text-xl self-start text-gray-700">
+                                        <h2 className="leading-relaxed">Create an Event</h2>
+                                        <p className="text-sm text-gray-500 font-normal leading-relaxed">Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
                                     </div>
                                 </div>
-                                <div className="flex flex-col">
-                                    <label className="leading-loose">Event Description</label>
-                                    <input type="text" className="px-4 py-2 border focus:ring-gray-500 focus:border-gray-900 w-full sm:text-sm border-gray-300 rounded-md focus:outline-none text-gray-600 bg-white" placeholder="Optional" />
-                                </div>
-                            </div>
-                            <div className="pt-4 flex items-center space-x-4">
-                                <button className="flex justify-center items-center w-full text-gray-900 px-4 py-3 rounded-md focus:outline-none" onClick={() => setOpenmodal(true)}>
-                                    <svg className="w-6 h-6 mr-3" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth ="2" d="M6 18L18 6M6 6l12 12"></path></svg> Cancel
-                                </button>
-                                <button className="bg-blue-500 flex justify-center items-center w-full text-white px-4 py-3 rounded-md focus:outline-none" >Create</button>
+                                <form onSubmit={AddToDoHandler}>
+
+
+                                    <div className="divide-y divide-gray-200">
+                                        <div className="py-6 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
+                                            <div className="flex flex-col">
+                                                <label className="leading-loose">Event Title</label>
+                                                <input value={Title} onChange={(e) => setTitle(e.target.value)} type="text" className="px-4 py-2 border focus:ring-gray-500 focus:border-gray-900 w-full sm:text-sm border-gray-300 rounded-md focus:outline-none text-gray-600 bg-white" placeholder="Event title" />
+                                            </div>
+                                            <div className="flex flex-col">
+                                                <label className="leading-loose">Event Subtitle</label>
+                                                <input value={Category} onChange={(e) => setCategory(e.target.value)} type="text" className="px-4 py-2 border focus:ring-gray-500 focus:border-gray-900 w-full sm:text-sm border-gray-300 rounded-md focus:outline-none text-gray-600 bg-white" placeholder="Optional" />
+                                            </div>
+                                            <div className="flex items-center space-x-4">
+                                                <div className="flex flex-col">
+                                                    <label className="leading-loose">Start</label>
+                                                    <div className="relative focus-within:text-gray-600 text-gray-400">
+                                                        <input value={ExpiratioDate} onChange={(e) => setExpiratioDate(e.target.value)} type="date"
+                                                            aria-label="Fecha" className="pr-4 pl-10 py-2 border focus:ring-gray-500 focus:border-gray-900 w-full sm:text-sm border-gray-300 rounded-md focus:outline-none text-gray-600 bg-white" placeholder="25/02/2020" />
+                                                    </div>
+                                                </div>
+                                                <div className="flex flex-col">
+                                                    <label className="leading-loose">End</label>
+                                                    <div className="relative focus-within:text-gray-600 text-gray-400">
+                                                        <input type="date"
+                                                            aria-label="Fecha" className="pr-4 pl-10 py-2 border focus:ring-gray-500 focus:border-gray-900 w-full sm:text-sm border-gray-300 rounded-md focus:outline-none text-gray-600 bg-white" placeholder="26/02/2020" />
+
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div className="flex flex-col">
+                                                <label className="leading-loose">Event Description</label>
+                                                <input value={Description} onChange={(e) => setDescription(e.target.value)} type="text" className="px-4 py-2 border focus:ring-gray-500 focus:border-gray-900 w-full sm:text-sm border-gray-300 rounded-md focus:outline-none text-gray-600 bg-white" placeholder="Optional" />
+                                            </div>
+                                        </div>
+                                        <div className="pt-4 flex items-center space-x-4">
+                                            <button className="flex justify-center items-center w-full text-gray-900 px-4 py-3 rounded-md focus:outline-none" onClick={() => setOpenmodal(true)}>
+                                                <svg className="w-6 h-6 mr-3" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12"></path></svg> Cancel
+                                            </button>
+                                            <button type='submit' className="bg-blue-500 flex justify-center items-center w-full text-white px-4 py-3 rounded-md focus:outline-none" >Create</button>
+                                        </div>
+                                    </div>
+                                </form>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-        </div>
-      
-    );
-   }
-    
+
+        );
+    }
+
 };
 
 export default YourComponent;
