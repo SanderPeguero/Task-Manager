95f5522666f2d7f6d839b72d26b5651283ea8e1f
jose22gonzalez <urenajosealbeto@gmail.com>
2023-11-27 10:08:41 -0400

    LogIn improved

--- src/layout/LogIn.jsx
+++ src/layout/LogIn.jsx
@@ -10,7 +10,7 @@ const LogIn = () => {
 
     const LogInHandler = async (e) => {
         e.preventDefault();
-       
+
         if ([email, password].includes("")) {
 
             toast.error("All fields are required",
@@ -33,7 +33,7 @@ const LogIn = () => {
             } else {
 
                 login(email, password)
-   
+
             }
         }
     }
@@ -41,9 +41,6 @@ const LogIn = () => {
         <div className="h-screen bg-gray-100 flex flex-col justify-center sm:py-12">
             <div className="p-10 xs:p-0 mx-auto md:w-full md:max-w-md">
                 <h1 className="font-bold text-center text-2xl mb-5">Your Logo</h1>
-
-
-
                 <div className="bg-white shadow w-full rounded-lg divide-y divide-gray-200">
                     <form onSubmit={LogInHandler}>
                         <div className="px-5 py-7">
@@ -78,60 +75,21 @@ const LogIn = () => {
                                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M17 8l4 4m0 0l-4 4m4-4H3" />
                                 </svg>
                             </button>
+                            <div className="text-right my-2">
+
+                                <Link to="/forgotpassword" className="text-gray-500 hover:text-sky-600 hover:underline transition-all">
+                                    Forgot password?
+                                </Link>
+                            </div>
+
                         </div>
                     </form>
-                    
-                    <div className="py-5">
-                        <div className="grid grid-cols-2 gap-1">
-                        <div className="text-center">
-                        Don't have an account? <Link to="/registro" className="text-sky-600 font-medium hover:underline transition-all">Sign In  </Link>
-                    </div>
-                            <div className="text-center sm:text-left whitespace-nowrap">
-                                <button className="transition duration-200 mx-5 px-5 py-4 cursor-pointer font-normal text-sm rounded-lg text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-200 focus:ring-2 focus:ring-gray-400 focus:ring-opacity-50 ring-inset">
-                                    <svg
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        fill="none"
-                                        viewBox="0 0 24 24"
-                                        stroke="currentColor"
-                                        className="w-4 h-4 inline-block align-text-top"
-                                    >
-                                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M8 11V7a4 4 0 118 0m-4 8v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2z" />
-                                    </svg>
-                                    <span className="inline-block ml-1">Forgot Password</span>
-                                </button>
+
+                    <div className="py-8">
+                        <div className="">
+                            <div className="text-center">
+                                Don't have an account? <Link to="/signln" className="text-sky-600 font-medium hover:underline transition-all">Sign In  </Link>
                             </div>
-                            <div className="text-center sm:text-right  whitespace-nowrap">
-                                <button className="transition duration-200 mx-5 px-5 py-4 cursor-pointer font-normal text-sm rounded-lg text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-200 focus:ring-2 focus:ring-gray-400 focus:ring-opacity-50 ring-inset">
-                                    <svg
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        fill="none"
-                                        viewBox="0 0 24 24"
-                                        stroke="currentColor"
-                                        className="w-4 h-4 inline-block align-text-bottom	"
-                                    >
-                                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M18.364 5.636l-3.536 3.536m0 5.656l3.536 3.536M9.172 9.172L5.636 5.636m3.536 9.192l-3.536 3.536M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-5 0a4 4 0 11-8 0 4 4 0 018 0z" />
-                                    </svg>
-                                    <span className="inline-block ml-1">Help</span>
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="py-5">
-                    <div className="grid grid-cols-2 gap-1">
-                        <div className="text-center sm:text-left whitespace-nowrap">
-                            <button className="transition duration-200 mx-5 px-5 py-4 cursor-pointer font-normal text-sm rounded-lg text-gray-500 hover:bg-gray-200 focus:outline-none focus:bg-gray-300 focus:ring-2 focus:ring-gray-400 focus:ring-opacity-50 ring-inset">
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    fill="none"
-                                    viewBox="0 0 24 24"
-                                    stroke="currentColor"
-                                    className="w-4 h-4 inline-block align-text-top"
-                                >
-                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 19l-7-7m0 0l7-7m-7 7h18" />
-                                </svg>
-                                <span className="inline-block ml-1">Back to your-app.com</span>
-                            </button>
                         </div>
                     </div>
                 </div>
